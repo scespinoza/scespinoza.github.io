19bf3bd1cb7f58906c1b1c7012b28f2bd4ac2f8e
scespinoza <secancinoe@gmail.com>
2022-09-13 23:51:51 -0300

    redesign page

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./projects/*.html", "./projects/*/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
